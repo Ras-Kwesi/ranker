d2c3bc828757619f3057801b2403d3e7a4259e8d
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-10-11 16:16:33 +0300

    structure made,user profile made added

---
+++ award/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(max_length=100, blank=True)
+    profilepic = models.ImageField(upload_to='picture/',blank=True)
+    contact = models.CharField(max_length=15,blank=True)
---
+++ award/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render, redirect
+from .models import *
+from .forms import *
+
+# Create your views here.
