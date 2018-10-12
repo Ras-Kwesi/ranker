d1923661fd17a23086ba1b371732e4ce1dd1ca00
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-10-12 13:00:41 +0300

    changes to urls and views

--- award/urls.py
+++ award/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     url('^$',views.index,name='index')
 ]
--- award/views.py
+++ award/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
 
+
 # Create your views here.
 
 def index(request):
