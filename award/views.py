6734016a256caa51cf4d11cc855a81b075306a4e
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-10-12 14:43:49 +0300

    profile view fnction added

--- award/views.py
+++ award/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -9,5 +10,16 @@ def index(request):
     projects = Project.objects.all()
 
     return render(request,'index.html',{'projects':projects})
+
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    profile = Profile.objects.get(user=current_user)
+    print(profile)
+    # profile = Profile.objects.filter(user=request.user.id)
+    projects = Project.objects.filter(profile = current_user)
+
+    return render(request, 'profile.html', {'profile': profile, 'images': projects})
 
 
