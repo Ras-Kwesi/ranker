c41a49ffa761a78e5952ba39fc109ce314347c46
Ras-Kwesi <kwesi.makonnen@gmail.com>
2018-10-12 15:11:31 +0300

    new post view function added

--- award/views.py
+++ award/views.py
@@ -23,3 +23,17 @@ def profile(request):
     return render(request, 'profile.html', {'profile': profile, 'images': projects})
 
 
+@login_required(login_url='/accounts/register/')
+def new_post(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewProject(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.profile = current_user
+            image.save()
+        return redirect('index')
+
+    else:
+        form = NewProject()
+    return render(request, 'new_post.html', {"form": form})
